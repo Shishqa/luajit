6b8a8ac4f6e5adf3a1dfe788949eb5975e17e3fb
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-04-03 23:17:33 +0300

    dump_callchain_lfunc

--- src/profile/callchain.c
+++ src/profile/callchain.c
@@ -6,8 +6,10 @@ void dump_callchain_lfunc(ProfileState* ps) {
   assert(ps != NULL);
 
   lua_State* L = gco2th(gcref(ps->g->cur_L));
+  assert(L != NULL);
 
   size_t dumpstr_len = 0;
+
   const char* stack_dump =
       luaJIT_profile_dumpstack(L, "F;", INT32_MIN, &dumpstr_len);
 
@@ -22,8 +24,8 @@ void dump_callchain_cfunc(ProfileState* ps) {
 
   for (ssize_t i = depth - 1; i >= 0; --i) {
     write_iobuf(&ps->obuf, names[i],
-              strlen(names[i]));  // FIXME definetely will cause massive
-                                  // profiling overhead
+                strlen(names[i]));  // FIXME definetely will cause massive
+                                    // profiling overhead
     write_iobuf(&ps->obuf, ";", 1);
   }
   write_iobuf(&ps->obuf, "\n", 1);
